2c54e03c88b48b2f61b698beb6bfb6839bdf1d4f
mericargac <mericargac@gmail.com>
2019-09-17 20:35:44 +0300

    feat: add date util

--- src/common/utils.jsx
+++ src/common/utils.jsx
@@ -8,3 +8,41 @@ export const getTimeString = date =>
   date && typeof date.toLocaleTimeString === 'function'
     ? date.toLocaleTimeString('en-US')
     : '';
+
+const addDays = (date, days) => {
+  const tempDate = new Date(date);
+  return tempDate.setDate(tempDate.getDate() + days);
+};
+
+const addMonth = (date, month) => {
+  const tempDate = new Date(date);
+  return tempDate.setMonth(tempDate.getMonth() + month);
+};
+
+const sliderDateObj = date => {
+  date.toDateString('en-US', { month: 'short', weekday: 'short' });
+
+  return {
+    day: date.toLocaleDateString('en-US', { day: '2-digit' }),
+    weekday: date
+      .toLocaleDateString('en-US', { weekday: 'short' })
+      .toUpperCase(),
+    month: date.toLocaleDateString('en-US', { month: 'short' }).toUpperCase(),
+  };
+};
+
+export const getDates = () => {
+  const dateArray = [];
+
+  let currentDate = new Date();
+  const stopDate = addMonth(currentDate, 1);
+
+  while (currentDate <= stopDate) {
+    const tempDate = new Date(currentDate);
+
+    dateArray.push({ dateValue: tempDate, ...sliderDateObj(tempDate) });
+    currentDate = addDays(currentDate, 1);
+  }
+
+  return dateArray;
+};
