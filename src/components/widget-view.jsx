3b70542e92d336b89bcd3a5201581ec249045a13
mericargac <mericargac@gmail.com>
2019-09-17 20:41:32 +0300

    feat: add custom modal design and content styles
    add day-picker example

--- src/components/widget-view.jsx
+++ src/components/widget-view.jsx
@@ -5,8 +5,13 @@ import AppointmentIcon from '@assets/icon_widgets_appointment.png';
 import PricingIcon from '@assets/icon_widgets_pricing.png';
 import PromotionsIcon from '@assets/icon_widgets_promotions.png';
 
+import CloseDialogIcon from '@assets/icon_dialog_close.png';
+
 import 'rodal/lib/rodal.css';
 import Rodal from 'rodal';
+
+import DayPicker from '@components/day-picker';
+import { COLORS } from '../common/colors';
 
 const WidgetViewWrapper = styled.div`
   position: fixed;
@@ -31,6 +36,67 @@ const ImageWrapper = styled.img`
 const OptionsWrapper = styled.div`
   width: 175px;
   margin: 100px auto;
+`;
+
+//
+// const CloseIcon = styled.div`
+//   width: 72px;
+//   height: 72px;
+//   background: red;
+//
+//   border-top-left-radius: 10px;
+// `;
+// const CloseInner = styled.div`
+//   width: 100px;
+//   height: 50px;
+//   background: white;
+//
+//   position: absolute;
+//   transform: rotate(-45deg);
+// `;
+
+const CustomRodal = styled(Rodal)`
+  .rodal-dialog {
+    padding: 0;
+    overflow: hidden;
+    border-radius: 15px;
+    box-shadow: 3px 3px 10px 3px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const CloseIcon = styled.img`
+  position: absolute;
+  left: 0;
+  top: 0;
+  cursor: pointer;
+`;
+
+const ModalContentWrapper = styled.div`
+  display: flex;
+  height: 100%;
+`;
+const ModalContentContainer = styled.div`
+  flex: 2;
+`;
+const ModalInformationContainer = styled.div`
+  flex: 1;
+  background-color: ${COLORS.ALABASTER};
+  border-left-width: 1px;
+  border-left-style: solid;
+  border-left-color: ${COLORS.MERCURY};
+`;
+
+const DialogCycle = styled.div`
+  position: absolute;
+  width: 30px;
+  height: 30px;
+  border-radius: 30px;
+  background-color: ${COLORS.MERCURY};
+
+  left: 585px;
+
+  top: ${props => (props.top ? '-15px' : 'auto')};
+  bottom: ${props => (props.bottom ? '-15px' : 'auto')};
 `;
 
 const WidgetView = () => {
@@ -98,11 +164,30 @@ const WidgetView = () => {
         <ImageWrapper onClick={() => setShowModal(true)} src={PromotionsIcon} />
       </WidgetViewWrapper>
 
-      <Rodal
+      <DayPicker />
+
+      <CustomRodal
         visible={showModal}
         animation="flip"
+        closeMaskOnClick={false}
+        showCloseButton={false}
+        width={900}
+        height={420}
         onClose={() => setShowModal(false)}
-      ></Rodal>
+      >
+        <CloseIcon
+          src={CloseDialogIcon}
+          role="button"
+          onClick={() => setShowModal(false)}
+        />
+        <DialogCycle top />
+        <DialogCycle bottom />
+
+        <ModalContentWrapper>
+          <ModalContentContainer />
+          <ModalInformationContainer />
+        </ModalContentWrapper>
+      </CustomRodal>
     </>
   );
 };
