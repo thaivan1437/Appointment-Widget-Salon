df4097344a8bc22ce3d05dd3d0dca08865b8ab72
mericargac <mericargac@gmail.com>
2019-11-17 21:46:49 +0300

    remove mock services list and change model

--- src/components/service-selection/service-selection.jsx
+++ src/components/service-selection/service-selection.jsx
@@ -6,123 +6,7 @@ import UncheckIcon from '@assets/icon_unchecked.png';
 import RadioOn from '@assets/radio_on.png';
 import RadioOf from '@assets/radio_off.png';
 
-// TODO: mock data should remove
-const serviceList = {
-  Hair: [
-    {
-      serviceId: 111,
-      serviceName: 'Hair 0',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 222,
-      serviceName: 'Hair 1',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 333,
-      serviceName: 'Hair 2',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 444,
-      serviceName: 'Hair 3',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 442,
-      serviceName: 'Hair 32',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 441,
-      serviceName: 'Hair 31',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-  ],
-  Nail: [
-    {
-      serviceId: 221,
-      serviceName: 'Nail 0',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 555,
-      serviceName: 'Nail 1',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 666,
-      serviceName: 'Nail 2',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 777,
-      serviceName: 'Nail 3',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 888,
-      serviceName: 'Nail 4',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-    {
-      serviceId: 999,
-      serviceName: 'Nail 5',
-      price: {
-        amount: 12.45,
-        currencyCode: 'USD',
-        currencySymbol: '$',
-      },
-    },
-  ],
-};
-
-const ServiceSelection = ({ initialValue = [], onServiceSelected }) => {
+const ServiceSelection = ({ initialValue = [], onServiceSelected, serviceList = {} }) => {
   const [selectedCategory, setSelectedCategory] = useState();
   const [selectedServicesIds, setSelectedServicesIds] = useState([]);
   const [selectedServices, setSelectedServices] = useState(initialValue || []);
@@ -134,7 +18,7 @@ const ServiceSelection = ({ initialValue = [], onServiceSelected }) => {
   }, []);
 
   useEffect(() => {
-    setSelectedServicesIds(selectedServices.map(service => service.serviceId));
+    setSelectedServicesIds(selectedServices.map(service => service.id));
 
     onServiceSelected(selectedServices);
   }, [selectedServices]);
@@ -164,13 +48,13 @@ const ServiceSelection = ({ initialValue = [], onServiceSelected }) => {
                 const tempSelectedServicesIds = [...selectedServicesIds];
                 const tempSelectedServices = [...selectedServices];
 
-                if (tempSelectedServicesIds.indexOf(service.serviceId) === -1) {
+                if (tempSelectedServicesIds.indexOf(service.id) === -1) {
                   if (tempSelectedServicesIds.length < 4) {
                     tempSelectedServices.push(service);
                   }
                 } else {
                   tempSelectedServices.splice(
-                    tempSelectedServicesIds.indexOf(service.serviceId),
+                    tempSelectedServicesIds.indexOf(service.id),
                     1
                   );
                 }
@@ -180,12 +64,12 @@ const ServiceSelection = ({ initialValue = [], onServiceSelected }) => {
             >
               <S.IconContainer
                 src={
-                  selectedServicesIds.indexOf(service.serviceId) !== -1
+                  selectedServicesIds.indexOf(service.id) !== -1
                     ? CheckIcon
                     : UncheckIcon
                 }
               />
-              <div>{service.serviceName}</div>
+              <div>{service.name}</div>
             </S.ServiceItem>
           ))}
       </S.ServiceServicesContainer>
