37d686ce30d028605aa9fb67756d2edd11870374
mericargac <mericargac@gmail.com>
2019-11-20 21:47:02 +0300

    add sentry

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import WidgetView from '@components/widget-view';
 import 'common/fonts.css';
 import httpUtil from 'common/HttpUtil';
+import * as Sentry from '@sentry/browser';
 
 window.initWidget = function(d, appId) {
   var rootElement = document.createElement('div');
@@ -17,6 +18,10 @@ window.initWidget = function(d, appId) {
     })
     .then(response => {
       const configData = response.data;
+
+      Sentry.init({
+        dsn: 'https://42b8f6c49ef64e5db61139711826584b@sentry.io/1468247',
+      });
 
       ReactDOM.render(
         <WidgetView widgetConfig={configData} appId={appId} />,
