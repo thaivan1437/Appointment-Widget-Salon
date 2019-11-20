c612853b5012a789c226f37bf4e280e66f452375
mericargac <mericargac@gmail.com>
2019-11-20 22:26:15 +0300

    Revert "update request an appointment button disabled style"
    This reverts commit a2d44df5f80591e6b758d023d79223ab7466af73.

--- src/index.js
+++ src/index.js
@@ -16,87 +16,16 @@ window.initWidget = function(d, appId) {
       url: `https://configs.widgets.salonmanager.net/${appId}.json`,
       // url: `https://s3-us-west-2.amazonaws.com/configs.widgets.salonmanager.net/${appId}.json`,
     })
-    .then(
-      response => {
-        const configData = response.data;
+    .then(response => {
+      const configData = response.data;
 
-        Sentry.init({
-          dsn: 'https://42b8f6c49ef64e5db61139711826584b@sentry.io/1468247',
-        });
+      Sentry.init({
+        dsn: 'https://42b8f6c49ef64e5db61139711826584b@sentry.io/1468247',
+      });
 
-        ReactDOM.render(
-          <WidgetView widgetConfig={configData} appId={appId} />,
-          document.getElementById('widget-root')
-        );
-      },
-      () => {
-        const configData = {
-          email: 'developer@salonmanager.net',
-          style: 'red',
-          widgetData: {
-            appointments: {
-              'Hair (kids)': [
-                {
-                  isSelected: false,
-                  id: 'J2BO3EXIE3VSWTXMEU5JGK2B',
-                  categoyId: 'ISAVKHURPAINIVIMVC563ZAK',
-                  name: 'Ombré',
-                  categoryName: 'Hair (kids)',
-                },
-                {
-                  isSelected: false,
-                  id: 'GGHBY6ACNZG3KCOLIJVTCO6O',
-                  categoyId: 'ISAVKHURPAINIVIMVC563ZAK',
-                  name: 'Haircut + Shampoo (girl)',
-                  categoryName: 'Hair (kids)',
-                },
-                {
-                  isSelected: false,
-                  id: 'J2BO3EXIE3VSWTXMEU5JGK2B',
-                  categoyId: 'ISAVKHURPAINIVIMVC563ZAK',
-                  name: 'Haircut (girl)',
-                  categoryName: 'Hair (kids)',
-                },
-                {
-                  isSelected: false,
-                  id: 'GGHBY6ACNZG3KCOLIJVTCO6O',
-                  categoyId: 'ISAVKHURPAINIVIMVC563ZAK',
-                  name: 'Haircut + Shampoo (girl)',
-                  categoryName: 'Hair (kids)',
-                },
-                {
-                  isSelected: false,
-                  id: 'J2BO3EXIE3VSWTXMEU5JGK2B',
-                  categoyId: 'ISAVKHURPAINIVIMVC563ZAK',
-                  name: 'Ombré',
-                  categoryName: 'Hair (kids)',
-                },
-                {
-                  isSelected: false,
-                  id: 'GGHBY6ACNZG3KCOLIJVTCO6O',
-                  categoyId: 'ISAVKHURPAINIVIMVC563ZAK',
-                  name: 'Haircut + Shampoo (girl)',
-                  categoryName: 'Hair (kids)',
-                },
-              ],
-            },
-            pricings: [{ test: 'test', amount: 10 }],
-            promotions: [{ test: 'test' }],
-          },
-          widgets: ['WIDGET_APPOINTMENT'],
-          position: 'BOTTOM_LEFT',
-          orientation: 'HORIZONTAL',
-        };
-        Sentry.init({
-          dsn: 'https://42b8f6c49ef64e5db61139711826584b@sentry.io/1468247',
-        });
-
-        setTimeout(() => {
-          ReactDOM.render(
-            <WidgetView widgetConfig={configData} appId={appId} />,
-            document.getElementById('widget-root')
-          );
-        });
-      }
-    );
+      ReactDOM.render(
+        <WidgetView widgetConfig={configData} appId={appId} />,
+        document.getElementById('widget-root')
+      );
+    });
 };
