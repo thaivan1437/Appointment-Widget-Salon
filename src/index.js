356fadd977f2353024e6ded546389be1d61407b8
Tansel Dinçkaya <dinckayatansel@gmail.com>
2019-11-25 22:03:52 +0200

    ie

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import * as Sentry from '@sentry/browser';
+import "core-js/es6/set"
+import "core-js/es6/map";
+import "raf/polyfill";
+import 'core-js/es6/string';
 
 
 Sentry.init({dsn: "https://cb9595338c8a4f559e9006bb42caac3f@sentry.io/1468398"});
