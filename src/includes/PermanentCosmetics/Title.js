5b4285e55873ea45f38474a66703642d4a7a1354
tung <daotungdtvt@gmail.com>
2020-09-05 19:22:06 +0700

    test build

--- src/includes/PermanentCosmetics/Title.js
+++ src/includes/PermanentCosmetics/Title.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import { Animated } from "react-animated-css";
 
 
