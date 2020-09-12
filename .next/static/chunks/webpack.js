98d9f7586bcc50939d3e4e33722d3d36c7d4a1e7
tung <daotungdtvt@gmail.com>
2020-09-12 17:19:58 +0700

    fix bugs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "62008ba1fb779026af91";
+/******/ 	var hotCurrentHash = "1acb1b3ad224423cd999";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
