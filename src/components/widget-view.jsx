c3bc75f61945b02c0820e9585ceb0f1d9d9a8a34
Nizam Gok <nizamgok@Nizams-MacBook-Pro-2.local>
2020-09-23 00:18:26 -0700

    removed dash from time-zone

--- src/components/widget-view.jsx
+++ src/components/widget-view.jsx
@@ -734,7 +734,7 @@ const WidgetView = ({ widgetConfig, appId }) => {
                     customerName: userName,
                     customerPhoneNumber: `+1${userPhone.replace(/[^\d]/g, '')}`,
                     numberOfPeople: userCount,
-                    timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+                    timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
                     date: getRequestDateString(selectedDate.dateValue),
                     preferTime1: getHourString(selectedTime1),
                     preferTime2: getHourString(selectedTime2),
