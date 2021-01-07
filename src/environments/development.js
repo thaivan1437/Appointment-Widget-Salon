0b3ff4e7f70ac6144a0c6b2cc5957ff24d7549fd
thaivan <thaivan1437@gmail.com>
2021-01-07 01:46:03 +0000

    update locationID

--- src/environments/development.js
+++ src/environments/development.js
@@ -1,7 +1,7 @@
 module.exports = {
   domainExtension: 'net',
-  widgetAppId: "bbd2ba60-4f5b-11eb-9138-5175beaef02a",
+  widgetAppId: "805955f0-11a0-11eb-b864-710db1594949",
   SENTRY_URL: 'https://cb9595338c8a4f559e9006bb42caac3f@o257892.ingest.sentry.io/1468398',
   CDN_URL: 'https://cdn.belmontbeautysalon.net',
-  locationID: '74dd9df0-3e0d-11eb-9002-f5497aeb7b32',
+  locationID: 'bbd2ba60-4f5b-11eb-9138-5175beaef02a',
 };
