0cf8fecd55c615c436a6a97fe44377a05e801a59
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2021-02-11 16:19:15 +0900

    clean up belmont website*

--- inject-widget.js
+++ inject-widget.js
@@ -1,26 +1,24 @@
-
 const path = require('path');
 const argv = require('yargs').argv;
 const replace = require('replace-in-file');
 
 const env = argv.buildEnv || 'development';
 const configPath = path.resolve(
-    __dirname,
-    'src',
-    'environments',
-    env + '.js'
+  __dirname,
+  'src',
+  'environments',
+  env + '.js'
 );
 
 
 const CONFIGS = require(configPath);
 
 const options = {
-    files: 'public/index.html',
-    from: '</body>',
-    to: '<script src="https://widgets.salonmanager.'+ CONFIGS.domainExtension +'/loader.js" data-sm="'+CONFIGS.locationID+'" defer></script></body>'
+  files: 'public/index.html',
+  from: '</body>',
+  to: '<script src="https://widgets.salonmanager.' + CONFIGS.domainExtension + '/loader.js" data-sm="' + CONFIGS.locationID + '" defer></script></body>'
 };
 
 try {
-    const results = replace.sync(options);
-} catch (error) {
-}
+  replace.sync(options);
+} catch (error) {}
