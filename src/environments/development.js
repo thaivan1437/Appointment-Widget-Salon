3b6d44e9a0b1f24877552b94b60948dafb1fca22
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2021-05-06 21:59:26 +0900

    Update widgets locationId

--- src/environments/development.js
+++ src/environments/development.js
@@ -2,6 +2,6 @@ module.exports = {
   domainExtension: 'net',
   SENTRY_URL: 'https://cb9595338c8a4f559e9006bb42caac3f@o257892.ingest.sentry.io/1468398',
   CDN_URL: 'https://cdn.belmontbeautysalon.net',
-  widgetAppId: '7c2c1af0-6d3d-11eb-b791-291e57f7f0dd',
-  locationID: '65a0a510-7055-11eb-b920-fb3ecd76526d',
+  widgetAppId: 'af1f4e50-a97e-11eb-bd84-a9ce6e3abe6d',
+  locationID: 'af1f4e50-a97e-11eb-bd84-a9ce6e3abe6d',
 };
