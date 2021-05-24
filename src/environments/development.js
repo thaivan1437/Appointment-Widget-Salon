77dec0ab4c6e6081c6e22bb865f438457b8f2181
sonnguyeninspirelife@gmail.com <sonnguyeninspirelife@gmail.com>
2021-05-24 09:41:22 +0700

    update locationId for dev

--- src/environments/development.js
+++ src/environments/development.js
@@ -3,5 +3,5 @@ module.exports = {
   SENTRY_URL: 'https://cb9595338c8a4f559e9006bb42caac3f@o257892.ingest.sentry.io/1468398',
   CDN_URL: 'https://cdn.belmontbeautysalon.net',
   widgetAppId: 'af1f4e50-a97e-11eb-bd84-a9ce6e3abe6d',
-  locationID: 'af1f4e50-a97e-11eb-bd84-a9ce6e3abe6d',
+  locationID: 'cd0fe6b0-bbdd-11eb-85e2-59e607567fc6',
 };
