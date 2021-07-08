da3d5bb546e9c2bc34e6fb33de9430560c5fe5db
Hanh D. TRAN <tranduchanh.ms@gmail.com>
2021-07-08 23:25:28 +0900

    Update locationId

--- src/environments/development.js
+++ src/environments/development.js
@@ -2,5 +2,5 @@ module.exports = {
   domainExtension: 'net',
   SENTRY_URL: 'https://cb9595338c8a4f559e9006bb42caac3f@o257892.ingest.sentry.io/1468398',
   CDN_URL: 'https://cdn.belmontbeautysalon.net',
-  locationID: '5054d9b0-d80b-11eb-b0cd-098539432f0f',
+  locationID: 'e9e463a0-dff6-11eb-a0ef-39077fcdce69',
 };
